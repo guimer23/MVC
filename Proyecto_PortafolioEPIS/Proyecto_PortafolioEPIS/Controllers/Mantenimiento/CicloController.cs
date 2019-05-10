f0132df759d2a6c08eeb53d3b836857ba840fa7f
GUIMER SENON COAQUIRA COAQUIRA <guicoaquirac@upt.pe>
2019-05-10 12:20:57 -0500

    Creacion de Controlador y Vista para Mantenimiento Ciclo

---
+++ Proyecto_PortafolioEPIS/Proyecto_PortafolioEPIS/Controllers/Mantenimiento/CicloController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_PortafolioEPIS.Controllers.Mantenimiento
+{
+    public class CicloController : Controller
+    {
+        // GET: Ciclo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
