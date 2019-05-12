d31b071796969f6ff063f74a5ad692137f23bfc1
GUIMER SENON COAQUIRA COAQUIRA <guicoaquirac@upt.pe>
2019-05-12 01:03:11 -0500

    Creacion de la Vista Plan de Estudio

---
+++ Proyecto_PortafolioEPIS/Proyecto_PortafolioEPIS/Controllers/CargaAcademicaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_PortafolioEPIS.Controllers
+{
+    public class CargaAcademicaController : Controller
+    {
+        // GET: CargaAcademica
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Accion Nuevo
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+    }
+}
---
+++ Proyecto_PortafolioEPIS/Proyecto_PortafolioEPIS/Controllers/Mantenimiento/PlanEstudioController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_PortafolioEPIS.Controllers.Mantenimiento
+{
+    public class PlanEstudioController : Controller
+    {
+        // GET: PlanEstudio
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+    }
+}
